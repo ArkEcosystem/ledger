4a3e7177959877f2bb586be204d2c1fe9671126e
fix <fx.thoorens@gmail.com>
2017-06-22 09:48:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ark Ledger
+Ark wallet application for Ledger Blue and Nano S
+
+Under development
+
+## Install dev env
+- Install ledger vagrant https://github.com/fix/ledger-vagrant
+- Clone this repo under `apps/`
+- Connect to the vagrant machine `vagrant ssh`
+- Move to the app `cd apps/ark-ledger`
+- Connect your ledger Nano S
+- Install the app `sh rebuild.sh`
+
+## TODO
+[x] Make derivation path working
+[x] Generate correct address
+[ ] Parse Serialised transactions
+[ ] Sign transaction
---
+++ src/arkParse.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+*   Ark Wallet
+*   (c) 2017 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
+#include "os.h"
+#include "cx.h"
+#include <stdbool.h>
+
+#define MAX_BIP32_PATH 10
+
+typedef enum parserStatus_e {
+    USTREAM_PROCESSING,
+    USTREAM_FINISHED,
+    USTREAM_FAULT
+} parserStatus_e;
+
+typedef struct txContent_t {
+    uint8_t type;
+    uint32_t timestamp;
+    uint8_t senderPublicKey[33];
+    uint8_t recipientId[21];
+    uint8_t vendorField[64];
+    uint64_t amount;
+    uint64_t fee;
+} txContent_t;
+
+parserStatus_e parseTx(uint8_t *data, uint32_t length, txContent_t *context);
