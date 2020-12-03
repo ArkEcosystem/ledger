a9e797288b35f722ccf583dedcfbd0c2fefc54de
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2020-12-03 17:19:10 -0500

    refactor(nanox): increase display text length

--- src/display/context.h
+++ src/display/context.h
@@ -32,26 +32,21 @@
 #include "platform.h"
 
 ////////////////////////////////////////////////////////////////////////////////
-static const size_t DISPLAY_CTX_OP_SIZE         = 18;
 static const size_t DISPLAY_CTX_TEXT_SIZE_EXT   = 256;
 
 #if defined(SUPPORTS_LARGE_OPERATIONS)
-    static const size_t DISPLAY_CTX_STEP_COUNT  = 35;
+    static const size_t DISPLAY_CTX_OP_SIZE     = 21;
+    static const size_t DISPLAY_CTX_STEP_COUNT  = 40;
     static const size_t DISPLAY_CTX_TITLE_SIZE  = DISPLAY_CTX_OP_SIZE - 1;
     static const size_t DISPLAY_CTX_TEXT_SIZE   = 130;
 #else
+    static const size_t DISPLAY_CTX_OP_SIZE     = 18;
     static const size_t DISPLAY_CTX_STEP_COUNT  = 5;
     static const size_t DISPLAY_CTX_TITLE_SIZE  = DISPLAY_CTX_OP_SIZE - 3;
     static const size_t DISPLAY_CTX_TEXT_SIZE   = 68;
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////
-// Display Storage
-//
-// NanoS - 704-byte-aligned
-// NanoX - 2496-byte-aligned
-//
-// ---
 typedef struct display_context_t {
     char    operation   [DISPLAY_CTX_OP_SIZE];
     char    title       [DISPLAY_CTX_STEP_COUNT] [DISPLAY_CTX_TITLE_SIZE];
